3e1f0909ba776025fdd60852a052934063047351
Hashir Mehmood <hashirmehmood@Hashirs-MacBook-Pro.local>
2026-04-29 06:37:01 +0500

    fix 1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['http://195.110.58.111:3001'],
 };
 
 export default nextConfig;
